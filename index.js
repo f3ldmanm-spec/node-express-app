32b96467b0bc752a61a3d25afaa007cf6cb44f4d
f3ldmanm-spec <f3ldman.m@yandex.ru>
2026-04-17 00:08:48 +0300

    Initial commit: Express app with Docker and GitHub Actions

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Небольшой "секрет", который мы будем менять, чтобы видеть обновления
+const message = "Hello from Node.js App! Version 2.0"; 
+
+app.get('/', (req, res) => {
+  res.send(`
+    <html>
+      <body style="font-family: sans-serif; text-align: center; margin-top: 50px;">
+        <h1>${message}</h1>
+        <p>Deployed with GitHub Actions + Watchtower!</p>
+        <p>Current time: ${new Date().toLocaleString()}</p>
+      </body>
+    </html>
+  `);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
